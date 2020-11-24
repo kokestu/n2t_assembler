687d261b0ba180699282926cf7f8c2e9229a2a2a
Jonathan <jonny.a.sutton@gmail.com>
2020-11-24 19:23:24 +0000

    implement some parsers

--- src/Grammar.hs
+++ src/Grammar.hs
@@ -2,7 +2,62 @@ module Grammar where
 
 type Program = [Line]
 
-type Line = Either AInstruction CInstruction
+data Line = LIn LInstruction
+          | AIn AInstruction 
+          | CIn CInstruction
 
-data AInstruction = Todo
-data CInstruction = AlsoTodo
+data VirtualRegister = R Int
+
+data PredefinedPointer = SP
+                        | LCL
+                        | ARG
+                        | THIS
+                        | THAT
+
+data IOPointer = Screen
+               | Kbd
+
+data Symbol = VR VirtualRegister
+            | PP PredefinedPointer
+            | IP IOPointer
+            | UDefSymbol String
+
+data LInstruction = Label String
+
+data AInstruction = AtNum Int
+                  | AtSymbol Symbol
+
+data Reg = D | A | M
+
+data DestReg = Single Reg
+             | Double Reg Reg
+             | Triple Reg Reg Reg
+             | RNull
+
+data ConstExpr = Zero
+               | One
+               | Register Reg
+ 
+data Expr = C ConstExpr
+          | Add ConstExpr ConstExpr
+          | Minus ConstExpr ConstExpr
+          | And ConstExpr ConstExpr
+          | Or ConstExpr ConstExpr
+          | Not ConstExpr
+
+
+data Assignment = Ass DestReg Expr
+
+data Jump = JMP
+          | JEQ
+          | JNE
+          | JLT
+          | JLE
+          | JGT
+          | JGE
+          | JNull
+
+data CInstruction = CAss Assignment
+                  | JAss Assignment Jump
+                  | JExpr Expr Jump
+
--- src/Parser.hs
+++ src/Parser.hs
@@ -4,8 +4,19 @@ import Text.Parsec
   ( Parsec
   , (<|>)
   , many
+  , many1
+  , between
+  , skipMany
+  , optional
   )
 import Text.Parsec.Char
+  ( char
+  , newline
+  , spaces
+  , anyChar
+  , string
+  , letter
+  )
 import Data.Maybe (catMaybes)
 
 import Grammar
@@ -16,15 +27,27 @@ parseProgram :: Parser [Line]
 parseProgram = catMaybes <$> many parseLine
 
 parseLine :: Parser (Maybe Line)
-parseLine = (Just . Left <$> aInstruction)
-         <|> (Just . Right <$> cInstruction)
+parseLine = (Just . AIn <$> aInstruction)
+         <|> (Just . CIn <$> cInstruction)
+         <|> (Just . LIn <$> lInstruction)
          <|> (maybeCommentOrWhitespace *> pure Nothing)
-
-maybeCommentOrWhitespace :: Parser ()
-maybeCommentOrWhitespace = undefined
+         <* newline
 
 aInstruction :: Parser AInstruction
 aInstruction = undefined
 
 cInstruction :: Parser CInstruction
 cInstruction = undefined
+
+lInstruction :: Parser LInstruction
+lInstruction = Label <$> between
+  (char '(')
+  (char ')')
+  (many1 letter)
+  <* maybeCommentOrWhitespace
+
+comment :: Parser ()
+comment = string "//" >> skipMany anyChar
+
+maybeCommentOrWhitespace :: Parser ()
+maybeCommentOrWhitespace = spaces *> optional comment
