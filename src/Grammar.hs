578555c07b2bc65afb2f716b6cad4df30dc97c82
Jonathan <jonny.a.sutton@gmail.com>
2020-11-25 15:59:25 +0000

    more parsers + build a partial symbol table on the parsing pass

--- src/Grammar.hs
+++ src/Grammar.hs
@@ -5,38 +5,45 @@ type Program = [Line]
 data Line = LIn LInstruction
           | AIn AInstruction 
           | CIn CInstruction
+          deriving (Show)
 
-data VirtualRegister = R Int
+data VirtualRegister = R Int deriving (Show)
 
 data PredefinedPointer = SP
                         | LCL
                         | ARG
                         | THIS
                         | THAT
+                        deriving (Show)
 
-data IOPointer = Screen
-               | Kbd
+data IOPointer = SCREEN
+               | KBD
+               deriving (Show)
 
 data Symbol = VR VirtualRegister
             | PP PredefinedPointer
             | IP IOPointer
             | UDefSymbol String
+            deriving (Show)
 
-data LInstruction = Label String
+data LInstruction = Label String deriving (Show)
 
-data AInstruction = AtNum Int
+data AInstruction = AtInt Int
                   | AtSymbol Symbol
+                  deriving (Show)
 
-data Reg = D | A | M
+data Reg = D | A | M deriving (Show)
 
 data DestReg = Single Reg
              | Double Reg Reg
              | Triple Reg Reg Reg
              | RNull
+             deriving (Show)
 
 data ConstExpr = Zero
                | One
                | Register Reg
+               deriving (Show)
  
 data Expr = C ConstExpr
           | Add ConstExpr ConstExpr
@@ -44,9 +51,10 @@ data Expr = C ConstExpr
           | And ConstExpr ConstExpr
           | Or ConstExpr ConstExpr
           | Not ConstExpr
+          deriving (Show)
 
 
-data Assignment = Ass DestReg Expr
+data Assignment = Ass DestReg Expr deriving (Show)
 
 data Jump = JMP
           | JEQ
@@ -56,8 +64,10 @@ data Jump = JMP
           | JGT
           | JGE
           | JNull
+          deriving (Show)
 
 data CInstruction = CAss Assignment
                   | JAss Assignment Jump
                   | JExpr Expr Jump
+                  deriving (Show)
 
--- src/Parser.hs
+++ src/Parser.hs
@@ -1,14 +1,6 @@
 module Parser where
 
-import Text.Parsec
-  ( Parsec
-  , (<|>)
-  , many
-  , many1
-  , between
-  , skipMany
-  , optional
-  )
+import Text.Parsec hiding (Line)
 import Text.Parsec.Char
   ( char
   , newline
@@ -16,15 +8,23 @@ import Text.Parsec.Char
   , anyChar
   , string
   , letter
+  , digit
+  , oneOf
   )
 import Data.Maybe (catMaybes)
+import Data.Map
 
 import Grammar
 
-type Parser = Parsec String ()
+-- Use a Map for the symbol table
+type Parser = Parsec String (Map String (Maybe Int))
+type SymbolTable = Map String (Maybe Int)
 
-parseProgram :: Parser [Line]
-parseProgram = catMaybes <$> many parseLine
+parseProgram :: Parser ([Line], SymbolTable)
+parseProgram = do
+  lines <- many parseLine
+  st <- getState
+  return (catMaybes lines, st)
 
 parseLine :: Parser (Maybe Line)
 parseLine = (Just . AIn <$> aInstruction)
@@ -34,17 +34,60 @@ parseLine = (Just . AIn <$> aInstruction)
          <* newline
 
 aInstruction :: Parser AInstruction
-aInstruction = undefined
+aInstruction = char '@' >> (parseInt <|> parseSymbol)  <* maybeCommentOrWhitespace
+  where
+    parseInt = AtInt <$> (read <$> many1 digit)
+    parseSymbol = AtSymbol <$> symbol
+
+symbol :: Parser Symbol
+symbol = VR <$> (try parseVR)
+        <|> PP <$> (try parsePP)
+        <|> IP <$> (try parseIP)  
+        <|> UDefSymbol <$> (try $ parseSymbolAndUpdateTable Nothing)
+
+parseSymbolAndUpdateTable :: Maybe Int -> Parser String
+parseSymbolAndUpdateTable x = do
+  symbol <- parseIdentifier
+  updateState $ insertWith (flip const) symbol x 
+  return symbol
+
+parseVR :: Parser VirtualRegister
+parseVR = char 'R' >> R <$> (read <$> many1 digit)
+
+parsePP :: Parser PredefinedPointer
+parsePP = (string "SP" *> pure SP)
+          <|> (string "LCL" *> pure LCL)
+          <|> (string "ARG" *> pure ARG)
+          <|> (string "THIS" *> pure THIS)
+          <|> (string "THAT" *> pure THAT)
+          
+
+parseIP :: Parser IOPointer
+parseIP = (string "SCREEN" *> pure SCREEN)
+        <|> (string "KBD" *> pure KBD)
+
+parseIdentifier :: Parser String
+parseIdentifier = do
+  firstChar <- nonDigitChar
+  rest <- maybeDigitCharList
+  return (firstChar:rest)
+  where
+    nonDigitChar = oneOf "_.$:" <|> letter
+    maybeDigitCharList = many (nonDigitChar <|> digit)
 
 cInstruction :: Parser CInstruction
-cInstruction = undefined
+cInstruction = string "this should never work!!1!"
+    >> (return $ CAss (Ass (Single D) (C Zero)))
 
 lInstruction :: Parser LInstruction
-lInstruction = Label <$> between
-  (char '(')
-  (char ')')
-  (many1 letter)
-  <* maybeCommentOrWhitespace
+lInstruction = Label <$> do
+  line <- Just <$> sourceLine <$> getPosition
+  label <- between
+    (char '(')
+    (char ')')
+    (parseSymbolAndUpdateTable line)
+  maybeCommentOrWhitespace
+  return label
 
 comment :: Parser ()
 comment = string "//" >> skipMany anyChar
